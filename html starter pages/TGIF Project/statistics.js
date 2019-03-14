f3309fe8b5958c38b9d89ab80c85130bda150809
Ami <ami.nakano@outlook.com>
2019-03-14 22:29:07 +0100

    statistics updated

--- html starter pages/TGIF Project/statistics.js
+++ html starter pages/TGIF Project/statistics.js
@@ -10,9 +10,13 @@ var statistics = {
   "number_of_Democrats": totalMembers('D'),
   "number_of_Republicans": totalMembers('R'),
   "number_of_Independent": totalMembers('I'),
-  "percent_voted_party_democrat":Number(demVote.toFixed(2)), //toFixed(2) is to throwaway small numbers after 2 degits from the dot
+  "percent_voted_party_democrat":Number(demVote.toFixed(2)),
   "percent_voted_party_republican":Number(repVote.toFixed(2)),
   "percent_voted_party_independent":Number(indVote.toFixed(2)),
+  'least_engaged_percent_missed':"",
+  'missed_votes_asc': tenPercent(sortArrayAscending("missed_votes"),"missed_votes"),
+    'missed_votes_desc': tenPercent(sortArrayDescending("missed_votes"),"missed_votes"),
+  'pct_votes_asc': tenPercent(sortArrayAscending("votes_with_party_pct"),"votes_with_party_pct") , 'pct_votes_desc': tenPercent(sortArrayDescending("votes_with_party_pct"),"votes_with_party_pct")
 }
 console.log(statistics);
 
@@ -28,7 +32,7 @@ function caliculateTotalVoteWithParty(party){
 }
 
 // results are the sum of each party's votes
-console.log(caliculateTotalVoteWithParty('R')); //4008.9499999999985
+// console.log(caliculateTotalVoteWithParty('R')); //4008.9499999999985
 caliculateTotalVoteWithParty('D');
 caliculateTotalVoteWithParty('I');
 
@@ -43,14 +47,51 @@ function totalMembers(party){
     }
   }return partyMembers.length;
 }
-console.log(totalMembers('R')); //46
 
-// caliculation votes devided by the numbers of members = percentage
-console.log(caliculateTotalVoteWithParty('R')/totalMembers('R'));//87.15108695652171
-console.log(caliculateTotalVoteWithParty('D')/totalMembers('D'));//96.97052631578948
-console.log(caliculateTotalVoteWithParty('I')/totalMembers('I'));//95.17500000000001
+// sort function - ascending
+function sortArrayAscending(key){
+  var attendanceArray = [];
+  for (var i =0; i < members.length; i++){
+    attendanceArray.push(members[i]);
+  }
+  attendanceArray.sort(function(a,b){
+    return a[key]-b[key];
+  });
+  return attendanceArray;
+
+}
+sortArrayAscending("missed_votes_pct");
+sortArrayAscending("missed_votes");
 
 
+
+// sort function - descending
+function sortArrayDescending(key){
+  var attendanceArray = [];
+  for (var i =0; i < members.length; i++){
+    attendanceArray.push(members[i]);
+  }
+  attendanceArray.sort(function(a,b){
+    return b[key]-a[key];
+  });
+  return attendanceArray;
+}
+sortArrayDescending();
+
+// get 10 percent
+function tenPercent(array, key){
+  var tenPercentArray = [];
+  for(var i = 0; i < array.length; i++){
+    if(i < (array.length*0.1)){
+      tenPercentArray.push(array[i]);
+    }else if(array[i][key]==array[i-1][key]){
+      tenPercentArray.push(array[i]);
+    }else{
+      break;
+    }
+  }
+  return tenPercentArray;
+}
 
 
 
